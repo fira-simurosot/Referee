5ed92b22fe5a1979dab4dbc7f8640fd55d6ac062
Peng Guanwen <pg999w@outlook.com>
2019-10-26 15:57:16 +0800

    Update proto files

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using FiraSim.Referee.Proto.SimToRef;
+using Grpc.Core;
 
-namespace Referee
+namespace FiraSim.Referee
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This project is still in early stage");
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Simulate.SimulateClient(channel);
+            var packet = new Packet();
+            var reply = client.simulate(packet);
+
+            channel.ShutdownAsync().Wait();
         }
     }
 }
