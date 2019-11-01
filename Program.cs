79b8b6bf01ef51a09748e44632934469ea46a76c
Peng Guanwen <pg999w@outlook.com>
2019-11-01 20:36:17 +0800

    Update FIRAMessage

--- Program.cs
+++ Program.cs
@@ -12,7 +12,17 @@ namespace FiraSim.Referee
             Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
 
             var client = new Simulate.SimulateClient(channel);
-            var packet = new Packet();
+            var packet = new Packet
+            {
+                Cmd = new Commands
+                {
+                    RobotCommands =
+                    {
+                        new Command { Id = 1, Yellowteam = false, WheelLeft = 0, WheelRight = 125 },
+                        new Command { Id = 2, Yellowteam = true, WheelLeft = 0, WheelRight = 125 },
+                    }
+                }
+            };
             var reply = client.simulate(packet);
 
             channel.ShutdownAsync().Wait();
