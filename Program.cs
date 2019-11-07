a38cde999f9772d640bdae25b1f915d63a73e0c9
Peng Guanwen <pg999w@outlook.com>
2019-11-07 16:54:46 +0800

    Use LINQ method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using FiraMessage.SimToRef;
 using Grpc.Core;
 using Referee.Simuro5v5;
@@ -38,70 +39,22 @@ namespace Referee
             {
                 RobotCommands =
                 {
-                    new Command { 
-                        Id = 0, 
+                    matchInfo.BlueRobots.Select((robot, i) => new Command
+                    {
+                        Id = (uint)i,
                         Yellowteam = false, 
-                        WheelLeft = matchInfo.BlueRobots[0].wheel.left, 
-                        WheelRight = matchInfo.BlueRobots[0].wheel.right 
-                    },
-                    new Command { 
-                        Id = 1, 
-                        Yellowteam = false, 
-                        WheelLeft = matchInfo.BlueRobots[1].wheel.left, 
-                        WheelRight = matchInfo.BlueRobots[1].wheel.right 
-                    },
-                    new Command { 
-                        Id = 2, 
-                        Yellowteam = false, 
-                        WheelLeft = matchInfo.BlueRobots[2].wheel.left, 
-                        WheelRight = matchInfo.BlueRobots[2].wheel.right 
-                    },
-                    new Command { 
-                        Id = 3, 
-                        Yellowteam = false, 
-                        WheelLeft = matchInfo.BlueRobots[3].wheel.left, 
-                        WheelRight = matchInfo.BlueRobots[3].wheel.right 
-                    },
-                    new Command { 
-                        Id = 4, 
-                        Yellowteam = false, 
-                        WheelLeft = matchInfo.BlueRobots[4].wheel.left, 
-                        WheelRight = matchInfo.BlueRobots[4].wheel.right 
-                    },
-                    
-                    new Command { 
-                        Id = 0, 
+                        WheelLeft = robot.wheel.left, 
+                        WheelRight = robot.wheel.right 
+                    }),
+                    matchInfo.YellowRobots.Select((robot, i) => new Command
+                    {
+                        Id = (uint)i,
                         Yellowteam = true, 
-                        WheelLeft = matchInfo.YellowRobots[0].wheel.left, 
-                        WheelRight = matchInfo.YellowRobots[0].wheel.right 
-                    },
-                    new Command { 
-                        Id = 1, 
-                        Yellowteam = true, 
-                        WheelLeft = matchInfo.YellowRobots[1].wheel.left, 
-                        WheelRight = matchInfo.YellowRobots[1].wheel.right 
-                    },
-                    new Command { 
-                        Id = 2, 
-                        Yellowteam = true, 
-                        WheelLeft = matchInfo.YellowRobots[2].wheel.left, 
-                        WheelRight = matchInfo.YellowRobots[2].wheel.right 
-                    },
-                    new Command { 
-                        Id = 3, 
-                        Yellowteam = true, 
-                        WheelLeft = matchInfo.YellowRobots[3].wheel.left, 
-                        WheelRight = matchInfo.YellowRobots[3].wheel.right 
-                    },
-                    new Command { 
-                        Id = 4, 
-                        Yellowteam = true, 
-                        WheelLeft = matchInfo.YellowRobots[4].wheel.left, 
-                        WheelRight = matchInfo.YellowRobots[4].wheel.right 
-                    }
+                        WheelLeft = robot.wheel.left, 
+                        WheelRight = robot.wheel.right 
+                    })
                 }
             };
         }
-        
     }
 }
