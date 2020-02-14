42368d6c7a982a28d1d22e813d0db3b54be20fd7
kian behzad <kian.behzad@gmail.com>
2020-02-14 17:25:39 +0330

    [clean] put time and step counters into gameControllerWidget class

--- gameContoller/RefServer.py
+++ gameContoller/RefServer.py
@@ -13,51 +13,18 @@ import common_pb2
 from PyQt5.QtWidgets import (QWidget, QGridLayout,QPushButton, QApplication, QLabel, QComboBox)
 from PyQt5.QtCore import QTimer
 
+##create gamecontroller GUI
+app = QApplication(sys.argv)
+myWidget = gameControllerWidget.GameControllerWidget()
 
+
+##initiate Referee communication
 class GameControllerServicer(service_pb2_grpc.GameControllerServicer):
 
     def handyReferee(self, request, context):
         print("handyReferee")
         response = messages_pb2.HandyRef()
         return response
-
-
-
-app = QApplication(sys.argv)
-myWidget = gameControllerWidget.GameControllerWidget()
-
-
-min = 0
-sec = 0
-def countTime():
-    global sec
-    global min
-    sec += 1
-    if sec == 60:
-        min += 1
-        sec = 0
-    myWidget.labelTimer.setText(str(min) + ':' + str(sec))
-
-
-step = 0
-timer = QTimer()
-timer.timeout.connect(countTime)
-
-
-
-def virtualGameControllerServicer():
-    global step
-    if step == 0:
-        timer.start(1000)
-    step += 1
-    request = messages_pb2.Environment()
-    # print(request.statistics.scoreYellow)
-    myWidget.labelsteper.setText('step ' + str(step) + ' / 18000')
-
-steper = QTimer()
-steper.timeout.connect(virtualGameControllerServicer)
-steper.start(16.6)
-
 
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 service_pb2_grpc.add_GameControllerServicer_to_server(GameControllerServicer(),server)
@@ -67,12 +34,16 @@ server.start()
 
 
 
+def virtualGameControllerServicer():
+    if myWidget.step == 0:
+        myWidget.startTimer()
+    myWidget.updateStats()
+    request = messages_pb2.Environment()
+    # print(request.statistics.scoreYellow)
 
-
-
-
-
-
+steper = QTimer()
+steper.timeout.connect(virtualGameControllerServicer)
+steper.start(16.6)
 
 
 
--- gameContoller/gameControllerWidget.py
+++ gameContoller/gameControllerWidget.py
@@ -2,12 +2,19 @@ import sys
 from PyQt5.QtWidgets import (QWidget, QGridLayout, QVBoxLayout, QPushButton, QApplication, QLabel, QComboBox, QLineEdit, QSizePolicy)
 from PyQt5.QtGui import QIcon
 from PyQt5 import uic
+from PyQt5.QtCore import QTimer
 
 
 
 class GameControllerWidget(QWidget):
     def __init__(self):
         super(GameControllerWidget, self).__init__()
+
+        self.timer = QTimer()
+        self.min = 0
+        self.sec = 0
+
+        self.step = 0
 
         self.createWidget()
         self.connections()
@@ -18,7 +25,7 @@ class GameControllerWidget(QWidget):
 
 
     def connections(self):
-        pass
+        self.timer.timeout.connect(self.handleTimer)
     #     self.pbleft.pressed.connect(self.pbleft_pressed)
     #     self.pbright.pressed.connect(self.pbright_pressed)
     #     self.pbup.pressed.connect(self.pbup_pressed)
@@ -35,8 +42,19 @@ class GameControllerWidget(QWidget):
     #
     #     self.lineSpeed.textChanged.connect(self.speed_changed)
 
+    def startTimer(self):
+        self.timer.start(1000)
 
+    def handleTimer(self):
+        self.sec += 1
+        if self.sec == 60:
+            self.min += 1
+            self.sec = 0
+        self.labelTimer.setText(str(self.min) + ':' + str(self.sec))
 
+    def updateStats(self):
+        self.step += 1
+        self.labelsteper.setText('step ' + str(self.step) + ' / 18000')
 
 
 if __name__ == '__main__':
